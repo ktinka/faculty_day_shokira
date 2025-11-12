efb6c5a65c5b2cc3dafb4cd376c5faa36b914b7b
ktinka <159685174+ktinka@users.noreply.github.com>
2025-11-12 23:15:58 +0300

    solved task_1.kt
    a solution of the task_1 in the lesson_1

---
+++ ru/tbank/education/school/git/task_1.kt
@@ -0,0 +1,16 @@
+fun main() {
+    fun sumEvenNumbers() {
+        println("Введите количество чисел:")
+        val num = readLine()!!.toInt()
+        var valCount = 0
+        for (i in 1..num) {
+            println("Введите число:")
+            val value_n = readLine()!!.toInt()
+            if (value_n % 2 == 0) {
+                valCount += value_n
+            }
+        }
+        println(valCount)
+    }
+    sumEvenNumbers()
+}
