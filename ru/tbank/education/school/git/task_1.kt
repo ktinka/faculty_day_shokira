2b922eb496a23efe3092331949770637179619b6
ktinka <159685174+ktinka@users.noreply.github.com>
2025-11-12 23:17:59 +0300

    changed task_1.kt

--- ru/tbank/education/school/git/task_1.kt
+++ ru/tbank/education/school/git/task_1.kt
@@ -1,16 +1,13 @@
-fun main() {
-    fun sumEvenNumbers() {
-        println("Введите количество чисел:")
-        val num = readLine()!!.toInt()
-        var valCount = 0
-        for (i in 1..num) {
-            println("Введите число:")
-            val value_n = readLine()!!.toInt()
-            if (value_n % 2 == 0) {
-                valCount += value_n
-            }
+fun sumEvenNumbers() {
+    println("Введите количество чисел:")
+    val num = readLine()!!.toInt()
+    var valCount = 0
+    for (i in 1..num) {
+        println("Введите число:")
+        val value_n = readLine()!!.toInt()
+        if (value_n % 2 == 0) {
+            valCount += value_n
         }
-        println(valCount)
     }
-    sumEvenNumbers()
+    println(valCount)
 }
