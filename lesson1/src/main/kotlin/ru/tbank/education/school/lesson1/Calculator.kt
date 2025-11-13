d9d153b4d7c128b1076e222152d48798db3e1b9c
ktinka <159685174+ktinka@users.noreply.github.com>
2025-11-13 08:59:35 +0300

    Update Calculator.kt
    add calculate()

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -3,8 +3,19 @@ package ru.tbank.education.school.lesson1
 /**
  * Метод для вычисления простых арифметических операций.
  */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+fun calculate(a: Double, b: Double, operation: OperationType = OperationType.ADD): Double? {
+    val resultOfCalculation = when (operation) {
+        OperationType.ADD -> (a + b)
+        OperationType.SUBTRACT -> (a - b)
+        OperationType.MULTIPLY -> (a * b)
+        OperationType.DIVIDE ->
+            if (b == 0.0) {
+                null
+            } else {
+                (a / b)
+            }
+    }
+    return resultOfCalculation
 }
 
 /**
